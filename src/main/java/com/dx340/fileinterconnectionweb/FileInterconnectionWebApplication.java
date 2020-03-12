f50d5a9591589bb08a1ee3fcb25673353f43f494
Zsk-d <daishaoshu@LOCAL.GCRJ.COM>
2020-03-12 15:58:16 +0800

    web代码新建

---
+++ src/main/java/com/dx340/fileinterconnectionweb/FileInterconnectionWebApplication.java
@@ -0,0 +1,13 @@
+package com.dx340.fileinterconnectionweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileInterconnectionWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileInterconnectionWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dx340/fileinterconnectionweb/FileInterconnectionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dx340.fileinterconnectionweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileInterconnectionWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
